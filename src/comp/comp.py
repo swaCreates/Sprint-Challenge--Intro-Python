7ebd9b72bf43f65bc04da75fad7ec18b15b363bd
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-06-12 21:00:14 -0400

    started files / currently working on cityreader file

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,7 +24,7 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [n for n in humans if n[0] == 'D']
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,29 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:
+        pass
+
+class GroundVehicle(Vehicle):
+        pass
+        #  inherits from Vehicle Class
+
+class Car(GroundVehicle):
+        pass
+        #  inherits from GroundVehicle
+
+class Motorcycle(GroundVehicle):
+        pass
+        #  inherits from GroundVehicle
+
+class FlightVehicle:
+        pass
+
+class Airplane(FlightVehicle):
+        pass
+        #  inherits from FlightVehicle
+
+class Starship:
+        pass
